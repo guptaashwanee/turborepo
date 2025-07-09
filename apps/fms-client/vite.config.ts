23c3295f56dfadae8625db23d5efbb5b53de7aef
Ashwanee Kumar Gupta <ashwanee@anscer.com>
2025-07-09 17:36:43 +0530

    :sparkles: feat(dependencies): add vite-plugin-mkcert and vite-tsconfig-paths

--- apps/fms-client/vite.config.ts
+++ apps/fms-client/vite.config.ts
@@ -1,22 +1,19 @@
-import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
-		TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+		tanstackRouter({ target: "react", autoCodeSplitting: true }),
 		react(),
 		tailwindcss(),
+		mkcert(),
+		tsconfigPaths(),
 	],
-	resolve: {
-		alias: {
-			"@": resolve(__dirname, "./src"),
-			"@anscer/ui": resolve(__dirname, "../../packages/ui/src"),
-		},
-	},
 	server: {
 		host: true,
 	},
